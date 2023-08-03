b9f43073bf3c2cb2e543726c3ba3d0d9528219e4
MAHMUT CELIK <mahmut.382@hotmail.com>
2023-08-03 20:09:41 +0300

    BS-1 INITIAL PACKAGES CREATED AND SWAGGER API ADDED TO PROJECT.

---
+++ backend/src/main/java/com/globe/gastronomy/backend/controller/MainApi.java
@@ -0,0 +1,14 @@
+package com.globe.gastronomy.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/main")
+public class MainApi {
+    @GetMapping
+    public String getMain(){
+        return "IN MAIN";
+    }
+}
